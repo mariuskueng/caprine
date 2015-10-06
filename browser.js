f672732a3e1e5f745bae7ada76f217e2484eb383
Marius Küng <maku@mailbox.org>
2015-10-06 13:00:06 +0200

    Fixed menu click events

--- browser.js
+++ browser.js
@@ -3,18 +3,18 @@ const ipc = require('ipc');
 
 ipc.on('show-preferences', () => {
 	// create the menu for the below
-	document.querySelector('._150g._30yy._2fug._p').click();
+	document.querySelector('.icon-menu').click();
 
-	document.querySelector('._54nq._2i-c._150g._558b._2n_z li:first-child a').click();
+	// document.querySelector('._54nq._2i-c._150g._558b._2n_z li:first-child a').click();
 });
 
 ipc.on('new-conversation', () => {
-	document.querySelector('._30yy._4kzv').click();
+	document.querySelector('.icon-chat').click();
 });
 
 ipc.on('log-out', () => {
 	// create the menu for the below
-	document.querySelector('._150g._30yy._2fug._p').click();
+	document.querySelector('.icon-menu').click();
 
-	document.querySelector('._54nq._2i-c._150g._558b._2n_z li:last-child a').click();
+	document.querySelector('.dropdown ul li:last-child a').click();
 });
